5d83a4b5eb39c9495e647adbb1aa88e5de89e5ab
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2021-11-02 15:26:23 +0700

    Setup

---
+++ Python/Discord Bot/discord bot tutorial/main.py
@@ -0,0 +1,34 @@
+#                        _oo0oo_
+#                       o8888888o
+#                       88" . "88
+#                       (| -_- |)
+#                       0\  =  /0
+#                     ___/`---'\___
+#                   .' \|     |// '.
+#                  / \|||  :  |||// \
+#                 / _||||| -:- |||||- \
+#                |   | \\  -  /// |   |
+#                | \_|  ''\---/''  |_/ |
+#                \  .-\__  '-'  ___/-. /
+#              ___'. .'  /--.--\  `. .'___
+#           ."" '<  `.___\_<|>_/___.' >' "".
+#          | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+#          \  \ `_.   \_ __\ /__ _/   .-` /  /
+#      =====`-.____`.___ \_____/___.-`___.-'=====
+#                        `=---='
+
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+
+client.run(TOKEN)
+
