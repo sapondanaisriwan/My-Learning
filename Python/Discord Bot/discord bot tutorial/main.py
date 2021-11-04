5706e59aac7aebba3a4c3f1f7de4a7036647d865
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2021-11-04 13:32:07 +0700

    Member join/Member leave

--- Python/Discord Bot/discord bot tutorial/main.py
+++ Python/Discord Bot/discord bot tutorial/main.py
@@ -19,40 +19,66 @@
 
 import os
 import discord
-from discord import channel
 from dotenv import load_dotenv
 from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-client = commands.Bot(command_prefix = '.')
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix='!', intents=intents)
+
+
+"""
+CLIENT EVENT
+"""
 
 @client.event
 async def on_ready():
     print('Bot is ready')
 
+# @client.event
+# async def on_message(message):
+#     client = message.channel
+#     if message.content.startswith('.ping'):
+#         await client.send('pong')
 
-@client.event
-async def on_message(message):
-    client = message.channel
-    print(type(message))
-    if message.content.startswith('.ping'):
-        await client.send('pong')
-
-    if message.content.startswith('.echo'):
-        msg = message.content.split() # ['.echo', 'hello', 'world', 'barrier']
-        msg = msg[1:] # ['hello', 'world', 'barrier']
-        output = ''
-        for word  in msg:
-            output += word
-            output += ' '
-        await client.send(output)
+#     if message.content.startswith('.echo'):
+#         msg = message.content.split() # ['.echo', 'hello', 'world', 'barrier']
+#         msg = msg[1:] # ['hello', 'world', 'barrier']
+#         output = ''
+#         for word  in msg:
+#             output += word
+#             output += ' '
+#         await client.send(output)
 
 # detect deleted message
 @client.event
 async def on_message_delete(message):
     content = message.content
     await message.channel.send(content)
+
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
+
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
+
+# @client.command(aliases=['clear'])
+# async def clear(ctx, amount = 100):
+    # print('here')
+    # channel = ctx.message.channel
+    # messages = []
+    # async for message in client.logs_form(channel, limit=int(amount)):
+    #     print(message)
+    #     messages.append(message)
+    # await client.delete_messasge(messages)
+    # await channel.send('test')
 
 
 client.run(TOKEN)
