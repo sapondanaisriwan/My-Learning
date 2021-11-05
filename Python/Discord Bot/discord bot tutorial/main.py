c1828f26ff07afdd716f9bd0188e35019a65b0c1
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2021-11-05 23:31:37 +0700

    nick/gay/ppsize/unban

--- Python/Discord Bot/discord bot tutorial/main.py
+++ Python/Discord Bot/discord bot tutorial/main.py
@@ -19,6 +19,9 @@
 
 import os
 import discord
+import random
+from datetime import datetime
+from discord import embeds
 from dotenv import load_dotenv
 from discord.ext import commands
 load_dotenv()
@@ -26,8 +29,7 @@ TOKEN = os.getenv('TOKEN')
 
 intents = discord.Intents.all()
 PREFIX = "!!"
-bot = commands.Bot(command_prefix=PREFIX, intents=intents, )
-
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 """
 Catch error message
@@ -36,17 +38,26 @@ Catch error message
 @bot.event
 async def on_command_error(ctx, error):
 
-    if isinstance(error, commands.CommandNotFound):
-        return
+    if isinstance(error, commands.MemberNotFound):
+        await ctx.send(f"MemberNotFound: {error}")
 
-    if isinstance(error, commands.MissingRequiredArgument):
+    elif isinstance(error, commands.CommandNotFound):
+        await ctx.send(f"Error: {error}")
+
+    elif isinstance(error, commands.CommandInvokeError):
+        await ctx.send(f'CommandInvokeError {error}')
+
+    elif isinstance(error, commands.MissingRequiredArgument):
         await ctx.send(f"Missing a required argument.")
 
-    if isinstance(error, commands.MissingPermissions):
+    elif isinstance(error, commands.MissingPermissions):
         await ctx.send("You do not have the permissions to run this command.")
     
-    if isinstance(error, commands.BotMissingPermissions):
+    elif isinstance(error, commands.BotMissingPermissions):
         await ctx.send("I don't have sufficient permissions!")
+
+    else:
+        print(error)
 
 
 """
@@ -65,7 +76,7 @@ async def on_message(message):
         return
     
     await bot.process_commands(message)
-    
+
 
 # detect deleted message
 @bot.event
@@ -108,8 +119,88 @@ async def kick(ctx, member: discord.Member, *, reason = None):
 @commands.has_permissions(ban_members = True)
 async def ban(ctx, member: discord.Member, *, reason = None):
     await member.ban(reason = reason)
-    await ctx.send(f'User {member} has been banned')
+    await ctx.send(f'User {member.mention} has been banned')
 
+
+@bot.command()
+@commands.has_permissions(ban_members = True)
+async def unban(ctx, *, member):
+
+    # [BanEntry(reason='fuck you', user=<User id=554181520286547999 name='Katozai' discriminator='5852' bot=False>)]
+    banned_users = await ctx.guild.bans() # get all the user who has gotten banned
+    member_name = member_discriminator = member_id = None
+
+    try:
+        member_name, member_discriminator = member.split('#') # member_discriminator = user's tag
+    except:
+        member_id = member
+
+    for ban_entry  in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator) or (user.id == int(member_id)):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
+
+@bot.command()
+@commands.has_permissions(manage_nicknames = True)
+async def nick(ctx, member: discord.Member, *, nickname):
+
+    embed = discord.Embed(
+        description=f'Nickname was changed to {member.mention}.'
+    )
+
+    await member.edit(nick = nickname)
+    await ctx.send(embed=embed)
+
+
+@bot.command(aliases=['reset', 'rsnick', 'resetnick'])
+@commands.has_permissions(manage_nicknames = True)
+async def reset_nick(ctx, member: discord.Member):
+
+    embed = discord.Embed(
+        description=f'Nickname was reset to {member.mention}.'
+    )
+
+    await member.edit(nick = None)
+    await ctx.send(embed=embed)
+
+
+@bot.command(aliases=['pp','ppsize', 'penissize'])
+async def _pennis_size(ctx, member: discord.Member):
+
+    if member.bot or ctx.author.bot:
+        return
+
+    pp_size = ['เล็กชิบหาย 🤏', 'เล็กชิบหาย 🤏', 'เล็กชิบหาย 🤏', 'เล็กชิบหาย 🤏', 'ปานกลาง 🙄', 'ปานกลาง 🙄',  'ใหญ่ 💪','โคตรใหญ่ 😱']
+    date = datetime.now().strftime("%x") # บอกวันเวลา
+
+    embed = discord.Embed(
+        title = 'ระบบคำนวณขนาดหำ',
+        description=f'{member.mention} หำ{random.choice(pp_size)}'
+    )
+
+    embed.set_footer(text = f"ID - {member.avatar} • {date}")
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def gay(ctx, member: discord.Member):
+
+    if member.bot or ctx.author.bot:
+        return
+
+    date = datetime.now().strftime("%x") # บอกวันเวลา
+
+    embed = discord.Embed(
+        title = 'ระบบคำนวณความเป็นเกย์',
+        description=f'{member.mention} เป็นเกย์ `{random.randint(0, 100)}%`'
+    )
+
+    embed.set_footer(text = f"ID - {member.avatar} • {date}")
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)
 
